7a5f1bc3859fdaf30956507defad5332ead02d08
joshpson <jpearson846@gmail.com>
2018-09-18 16:16:13 -0400

    basic get request

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Hello World");
-console.error("Woops");
+const https = require("https");
+
+function printMessage(username, badgeCount, points) {
+  `${username} has ${badgeCount} total badges and ${points} points in JavaScript`;
+}
+
+const request = https.get("https://teamtreehouse.com/joshpson.json", resp => {
+  console.dir(resp);
+});
+
