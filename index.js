bf5cab5bc8f924ba9fd076739e178c326fa8802d
joshpson <jpearson846@gmail.com>
2018-09-18 16:24:36 -0400

    working with response.on

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ function printMessage(username, badgeCount, points) {
   `${username} has ${badgeCount} total badges and ${points} points in JavaScript`;
 }
 
-const request = https.get("https://teamtreehouse.com/joshpson.json", resp => {
-  console.dir(resp);
-});
-
+const request = https.get(
+  "https://teamtreehouse.com/joshpson.json",
+  response => {
+    let body = "";
+    let count = 0;
+    response.on("data", data => {
+      body += data.toString();
+      count += 1;
+    });
+    response.on("end", () => console.log(body, count));
+  }
+);
